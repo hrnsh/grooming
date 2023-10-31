17a4373a29c73eec809ccea24579c25fd02000a7
Hero <cho021456@naver.com>
2023-10-31 11:38:25 +0900

    Revert "취소할 커밋"
    This reverts commit 8fbf415671b7af9db9b199732ac6eec0aaa08179.

--- src/main/java/kr/co/gudi/admin/controller/AdminController.java
+++ src/main/java/kr/co/gudi/admin/controller/AdminController.java
@@ -1,20 +1,5 @@
 package kr.co.gudi.admin.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
 public class AdminController {
-	Logger logger = LoggerFactory.getLogger(getClass());
-	
-	
-	
-	@RequestMapping(value = "/")
-	public String home() {
-		logger.info("home");
-		
-		return "home";
-	}
+
 }
