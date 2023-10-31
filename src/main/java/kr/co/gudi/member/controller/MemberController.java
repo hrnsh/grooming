2b47222139c05fa85710cacc4330384fbb40d219
kim_min_soo <imlapael@gmail.com>
2023-10-31 09:40:00 +0900

    Member logger&Autowired

--- src/main/java/kr/co/gudi/member/controller/MemberController.java
+++ src/main/java/kr/co/gudi/member/controller/MemberController.java
@@ -1,5 +1,20 @@
 package kr.co.gudi.member.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import kr.co.gudi.member.service.MemberService;
+
+@Controller
 public class MemberController {
 
+	Logger logger = LoggerFactory.getLogger(getClass());
+	
+	@Autowired MemberService service;
+	
+	
+	
+	
 }
--- src/main/java/kr/co/gudi/member/service/MemberService.java
+++ src/main/java/kr/co/gudi/member/service/MemberService.java
@@ -1,5 +1,17 @@
 package kr.co.gudi.member.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.co.gudi.member.dao.MemberDAO;
+
+@Service
 public class MemberService {
 
+	Logger logger = LoggerFactory.getLogger(getClass());
+	
+	@Autowired MemberDAO dao;
+	
 }
