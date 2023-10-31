8fbf415671b7af9db9b199732ac6eec0aaa08179
Hero <cho021456@naver.com>
2023-10-31 11:38:15 +0900

    취소할 커밋

--- src/main/java/kr/co/gudi/admin/controller/AdminController.java
+++ src/main/java/kr/co/gudi/admin/controller/AdminController.java
@@ -1,5 +1,20 @@
 package kr.co.gudi.admin.controller;
 
-public class AdminController {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class AdminController {
+	Logger logger = LoggerFactory.getLogger(getClass());
+	
+	
+	
+	@RequestMapping(value = "/")
+	public String home() {
+		logger.info("home");
+		
+		return "home";
+	}
 }
