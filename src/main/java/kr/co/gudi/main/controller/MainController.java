bf17be4527d710bd0f9f0f21e8a65a47dc279c55
amy <tjfdbfls12@naver.com>
2023-10-31 11:38:56 +0900

    Revert "취소"
    This reverts commit 6058a13e15d6909a5484cfc924ba1e78b3b78feb.

--- src/main/java/kr/co/gudi/main/controller/MainController.java
+++ src/main/java/kr/co/gudi/main/controller/MainController.java
@@ -1,17 +1,5 @@
 package kr.co.gudi.main.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
 public class MainController {
-	Logger logger = LoggerFactory.getLogger(getClass());
-	
-	@RequestMapping(value="/")
-	public String home() {
-		logger.info("home 페이지");
-		return "home";
-	}
+
 }
