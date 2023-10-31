6058a13e15d6909a5484cfc924ba1e78b3b78feb
amy <tjfdbfls12@naver.com>
2023-10-31 11:38:47 +0900

    취소

--- src/main/java/kr/co/gudi/main/controller/MainController.java
+++ src/main/java/kr/co/gudi/main/controller/MainController.java
@@ -1,5 +1,17 @@
 package kr.co.gudi.main.controller;
 
-public class MainController {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class MainController {
+	Logger logger = LoggerFactory.getLogger(getClass());
+	
+	@RequestMapping(value="/")
+	public String home() {
+		logger.info("home 페이지");
+		return "home";
+	}
 }
