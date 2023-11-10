9cf3ea74775878eec893ef0eff52d7f1c2911629
yongha <yong1807@gmail.com>
2023-11-10 17:43:05 +0900

    회원처리 저장

--- src/main/java/kr/co/gudi/admin/dao/ManageDAO.java
+++ src/main/java/kr/co/gudi/admin/dao/ManageDAO.java
@@ -1,11 +1,20 @@
 package kr.co.gudi.admin.dao;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import kr.co.gudi.admin.dto.ManageDTO;
 
 public interface ManageDAO {
 
-	ArrayList<ManageDTO> adList();
+	ArrayList<ManageDTO> adList(int offset);
+
+	int totalPage();
+
+	ArrayList<ManageDTO> adStateList(String user_state, int offset);
+
+	ArrayList<ManageDTO> manageDetail(String user_id);
+
+	void saveManage(HashMap<String, Object> params);
 
 }
--- src/main/java/kr/co/gudi/admin/service/ManageService.java
+++ src/main/java/kr/co/gudi/admin/service/ManageService.java
@@ -1,7 +1,11 @@
 package kr.co.gudi.admin.service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,11 +14,39 @@ import kr.co.gudi.admin.dto.ManageDTO;
 
 @Service
 public class ManageService {
-
+	Logger logger = LoggerFactory.getLogger(getClass());
 	@Autowired ManageDAO dao;
-	public ArrayList<ManageDTO> adList() {
+	public HashMap<String, Object> adList(String user_state, String page) {
+		int p = Integer.parseInt(page);		
+
+		int offset = (p-1)*10;		
+		ArrayList<ManageDTO> list = new ArrayList<ManageDTO>();
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		if(user_state.equals("전체")) {
+			list =dao.adList(offset);
+		}else {
+			list = dao.adStateList(user_state,offset);
+		}		
+		map.put("list",list);
 		
-		return dao.adList();
+		int pages = dao.totalPage();
+		//logger.info("만들 수 있는 총 페이지 수 : " +pages);
+		map.put("pages", pages);
+		// 만약 현재 보고있는 페이지가 총 페이지 수보다 크면 현재 페이지를 총 페이지 수로 변경한다.
+		if(p>pages) {
+			p=pages;
+		}
+		map.put("currPage", p);
+		return map;
+	}
+	public ArrayList<ManageDTO> manageDetail(String user_id) {
+	
+		return dao.manageDetail(user_id);
+	}
+	public void saveManage(HashMap<String, Object> params) {
+		dao.saveManage(params);
+		
+		
 	}
 
 }
